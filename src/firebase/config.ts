bb5ec1237c4d5420fede3e6a2c27042b7ca438a1
John Munez <account@lithiumtech.co>
2025-12-04 22:20:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5237367634-6c3d1",
+  "appId": "1:879923484742:web:a25b0001b852fb3999d247",
+  "apiKey": "AIzaSyDiKLHFBViZHYwB4NCZ4k8g25BvtAeYI4U",
+  "authDomain": "studio-5237367634-6c3d1.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "879923484742"
+};
