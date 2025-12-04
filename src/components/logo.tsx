024bae1ce7278f37707c2e52943bc2c23d732b2b
John Munez <account@lithiumtech.co>
2025-12-04 23:27:35 +0000

    Use this as a logo /home/user/studio/public/KlaroGov Logo.png

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export function Logo({ className }: { className?: string }) {
   return (
     <Image
-      src="/logo.svg"
+      src="/KlaroGov Logo.png"
       alt="KlaroGov Logo"
       width={32}
       height={32}
